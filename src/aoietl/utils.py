3507813bee9448976eb5c55289022bafad3ce9f3
GISRedeDev <dm@gisrede.com>
2025-06-06 16:54:00 +0100

    Initial commit and start datatypes

---
+++ src/aoietl/utils.py
@@ -0,0 +1,15 @@
+import yaml
+from .data_types import (
+    BronzeType,
+    SilverType,
+    GoldType,
+    PlatinumType,
+    RasterType,
+    HDFType,
+    VectorType,
+    ParquetType,
+    DirectoryType,
+    DataConfig,
+ 
+
+)
